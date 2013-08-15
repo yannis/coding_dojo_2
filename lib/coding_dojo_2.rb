77d027f14c2deec3eb665278b04f3233f9d32f26
Dominique Broeglin <dominique.broeglin@gmail.com>
2013-08-16 00:44:05 +0200

    Simple but not so efficient solution

--- lib/coding_dojo_2.rb
+++ lib/coding_dojo_2.rb
@@ -2,8 +2,33 @@ require "coding_dojo_2/version"
 
 module CodingDojo2
   class Potter
+    DISCOUNTS = [0, 1, 0.95, 0.90, 0.80, 0.75]
+
     def calculate(books)
-      return books.length * 8
+      return 0 if books.empty?
+      uniques, rest = pick_uniques(books)
+      split(uniques).map { |head, tail|
+        price(head.length) + calculate(tail.concat(rest))
+      }.min
+    end
+
+    def price(nb_uniq_books)
+      nb_uniq_books * 8 * DISCOUNTS[nb_uniq_books]
+    end
+
+    def pick_uniques(books)
+      uniques = []
+      rest = []
+      books.each { |book|
+        (uniques.include?(book) ? rest : uniques) << book
+      }
+      [uniques, rest]
+    end
+
+    def split(uniques)
+      (1..uniques.length).map { |pos|
+        [uniques.take(pos), uniques.drop(pos)]
+      }
     end
   end
 end
--- spec/coding_dojo_2_spec.rb
+++ spec/coding_dojo_2_spec.rb
@@ -5,7 +5,7 @@ describe CodingDojo2::Potter do
     subject.calculate([]).should == 0
   end
 
-  pending "should cost 8 for 1 book" do
+  it "should cost 8 for 1 book" do
     subject.calculate([0]).should == 8
     subject.calculate([1]).should == 8
     subject.calculate([2]).should == 8
@@ -13,48 +13,51 @@ describe CodingDojo2::Potter do
     subject.calculate([4]).should == 8
   end
 
-  pending "should discount nothing for identical books" do
+  it "should discount nothing for identical books" do
     subject.calculate([0, 0]).should == 8 * 2
     subject.calculate([1, 1, 1]).should == 8 * 3
   end
 
-  pending "should discount 5% for 2 different books" do
+  it "should discount 5% for 2 different books" do
     subject.calculate([0, 1]).should == 8 * 2 * 0.95
   end
 
-  pending "should discount 10% for 3 different books" do
+  it "should discount 10% for 3 different books" do
     subject.calculate([0, 2, 4]).should == 8 * 3 * 0.9
   end
 
-  pending "should discount 20% for 4 different books" do
+  it "should discount 20% for 4 different books" do
     subject.calculate([0, 1, 2, 4]).should == 8 * 4 * 0.8
   end
 
-  pending "should discount 25% for 5 different books" do
+  it "should discount 25% for 5 different books" do
     subject.calculate([0, 1, 2, 3, 4]).should == 8 * 5 * 0.75
   end
 
-  pending "should discount 5% for 2 different books out of 3" do
+  it "should discount 5% for 2 different books out of 3" do
     subject.calculate([0, 0, 1]).should == 8 + (8 * 2 * 0.95)
   end
 
-  pending "should discount 5% for 2 pairs of different books" do
+  it "should discount 5% for 2 pairs of different books" do
     subject.calculate([0, 0, 1, 1]).should == 2 * (8 * 2 * 0.95)
   end
 
-  pending "should discount 5% and 20% for 2 and 4 different books" do
+  it "should discount 5% and 20% for 2 and 4 different books" do
     subject.calculate([0, 0, 1, 2, 2, 3])
       .should == (8 * 2 * 0.95) + (8 * 4 * 0.8)
   end
 
-  pending "should discount 25% and nothing for 5 and 1 different books" do
+  it "should discount 25% and nothing for 5 and 1 different books" do
     subject.calculate([0, 1, 1, 2, 3, 4])
       .should == 8 + (8 * 5 * 0.75)
   end
 
-  pending "should be clever about grouping different books" do
+  it "should be clever about grouping different books" do
     subject.calculate([0, 0, 1, 1, 2, 2, 3, 4])
       .should == 2 * (8 * 4 * 0.8)
+  end
+
+  it "should be clever about grouping numerous different books" do
     subject.calculate([
       0, 0, 0, 0, 0,
       1, 1, 1, 1, 1,
@@ -62,5 +65,16 @@ describe CodingDojo2::Potter do
       3, 3, 3, 3, 3,
       4, 4, 4, 4])
       .should == 3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8)
+  end
+
+  it "should split properly" do
+    subject.split([1]).should == [[[1], []]]
+    subject.split([1, 2]).should == [[[1], [2]], [[1, 2], []]]
+    subject.split([1, 2]).should == [[[1], [2]], [[1, 2], []]]
+    subject.split([1, 2, 3]).should == [
+      [[1],       [2, 3]],
+      [[1, 2],    [3]],
+      [[1, 2, 3], []],
+    ]
   end
 end
